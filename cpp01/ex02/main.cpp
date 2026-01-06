027297cd0407b7a3fd9eb758ac4ed419e097ee50
Muhammed Soysal <soysalmuhammed000@gmail.com>
2026-01-06 19:35:58 +0300

    Refactor: Change Weapon member in HumanA from value to reference

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -3,25 +3,22 @@
 
 int main()
 {
-	// 1) Orijinal string (stack üzerinde)
 	std::string str = "HI THIS IS BRAIN";
-
-	// 2) Pointer: str'nin adresini tutar
+	
 	std::string *stringPTR = &str;
-
-	// 3) Reference: str'nin bir takma adı (alias)
 	std::string &stringREF = str;
 
-	// --- Adresleri yazdırma ---
+	// --- Write addresses ---
 	std::cout << "Address of str:      " << &str << std::endl;
 	std::cout << "Address held by PTR: " << stringPTR << std::endl;
 	std::cout << "Address held by REF: " << &stringREF << std::endl;
 
 	std::cout << std::endl;
 
-	// --- Değerleri yazdırma ---
+	// --- Write values ---
 	std::cout << "Value of str:        " << str << std::endl;
 	std::cout << "Value via PTR:       " << *stringPTR << std::endl;
 	std::cout << "Value via REF:       " << stringREF << std::endl;
+	
 	return 0;
 }
